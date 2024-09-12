bd6d650021fcfd4b2c2830a8e3dae356d112a22b
Plashon <fourza12poobate@gmail.com>
2024-09-12 17:20:36 +0700

    push

--- frontend/financialTracker/src/pages/dashboard/FinancialTable.jsx
+++ frontend/financialTracker/src/pages/dashboard/FinancialTable.jsx
@@ -5,7 +5,7 @@ import { useUser } from "@clerk/clerk-react";
 
 
 const FinancialTable = ({ financials }) => {
-  const { financials,deleteFinancial } = useFinancialRecord(); // ใช้ context
+  const { deleteFinancial } = useFinancialRecord(); // ใช้ context
   const { user } = useUser();
 
   const handleDelete = async (id) => {
